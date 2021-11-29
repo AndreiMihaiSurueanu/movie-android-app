8cc45f8c584593e1ae864f47250e212a033de3e2
andrei <git1@email.com>
2021-11-29 19:03:20 +0100

    Added Button to MainActivity

--- app/src/main/java/com/example/movieapp/MovieListActivity.java
+++ app/src/main/java/com/example/movieapp/MovieListActivity.java
@@ -3,12 +3,15 @@ package com.example.movieapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MovieListActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
     }
 }
