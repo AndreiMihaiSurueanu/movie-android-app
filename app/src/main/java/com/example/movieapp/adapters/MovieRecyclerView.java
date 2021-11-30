f980c0e0260dd246f3e6b1879ca5fbb325088a3b
andrei <git1@email.com>
2021-11-30 16:04:23 +0100

    Created MovieRecyclerView Adapter

---
+++ app/src/main/java/com/example/movieapp/adapters/MovieRecyclerView.java
@@ -0,0 +1,23 @@
+package com.example.movieapp.adapters;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MovieRecyclerView extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int i) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
