1c63c0bb1155bf8bccf5c5f60dc6e1350c82a7a5
andrei <git1@email.com>
2021-11-30 18:17:41 +0100

    Inflating Layout
    * Adding implementation to onCreateViewHolder method inside the MovieRecyclerView class

--- app/src/main/java/com/example/movieapp/adapters/MovieRecyclerView.java
+++ app/src/main/java/com/example/movieapp/adapters/MovieRecyclerView.java
@@ -1,15 +1,29 @@
 package com.example.movieapp.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.movieapp.R;
+import com.example.movieapp.models.MovieModel;
+
+import java.util.List;
+
 public class MovieRecyclerView extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<MovieModel> mMovies;
+    private OnMovieListener onMovieListener;
+
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_list_item, parent, false);
+        return new MovieViewHolder(view, onMovieListener);
+
     }
 
     @Override
