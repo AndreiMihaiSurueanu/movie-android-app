0fdee2c910af9acb438206ce8dbac8fe2184d008
andrei <git1@email.com>
2021-11-30 20:26:10 +0100

    Get PosterPath in MovieRecyclerView Adapter

--- app/src/main/java/com/example/movieapp/adapters/MovieRecyclerView.java
+++ app/src/main/java/com/example/movieapp/adapters/MovieRecyclerView.java
@@ -43,7 +43,7 @@ public class MovieRecyclerView extends RecyclerView.Adapter<RecyclerView.ViewHol
 
         // ImageView: Using Glide Library
         Glide.with(holder.itemView.getContext())
-                .load(mMovies.get(i))
+                .load(mMovies.get(i).getPoster_path())
                 .into(((MovieViewHolder)holder).imageView);
 
 
