81db064040eda2b2e68716d1284e8dda3655e7ec
andrei <git1@email.com>
2021-11-30 10:31:12 +0100

    Moved LiveData from MovieListViewModel to MovieRepository

--- app/src/main/java/com/example/movieapp/repositories/MovieRepository.java
+++ app/src/main/java/com/example/movieapp/repositories/MovieRepository.java
@@ -1,6 +1,15 @@
 package com.example.movieapp.repositories;
 
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.movieapp.models.MovieModel;
+
+import java.util.List;
+
 public class MovieRepository {
     // This class is acting as repositories
+
+    // Live Data
+    private MutableLiveData<List<MovieModel>> mMovies = new MutableLiveData<>();
 
 }
--- app/src/main/java/com/example/movieapp/viewmodels/MovieListViewModel.java
+++ app/src/main/java/com/example/movieapp/viewmodels/MovieListViewModel.java
@@ -1,7 +1,6 @@
 package com.example.movieapp.viewmodels;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.movieapp.models.MovieModel;
@@ -12,8 +11,6 @@ public class MovieListViewModel extends ViewModel {
 
     // this class is used for VIEWMODEL
 
-    // Live Data
-    private MutableLiveData<List<MovieModel>> mMovies = new MutableLiveData<>();
 
     // Constructor
     public MovieListViewModel() {
