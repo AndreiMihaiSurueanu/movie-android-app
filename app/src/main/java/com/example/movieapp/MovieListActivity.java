06157b00a88990a5368b41c9bffab7eaf6bb37ba
andrei <git1@email.com>
2021-11-30 21:49:54 +0100

    Commented out Retrofit methods
    * Commented out GetRetrofitResponse & GetRetrofitResponseAccordingToID methods from MovieListActivity

--- app/src/main/java/com/example/movieapp/MovieListActivity.java
+++ app/src/main/java/com/example/movieapp/MovieListActivity.java
@@ -1,27 +1,16 @@
 package com.example.movieapp;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-
 import com.example.movieapp.models.MovieModel;
-import com.example.movieapp.request.Servicey;
-import com.example.movieapp.response.MovieSearchResponse;
-import com.example.movieapp.utils.Credentials;
-import com.example.movieapp.utils.MovieApi;
 import com.example.movieapp.viewmodels.MovieListViewModel;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -55,75 +44,75 @@ public class MovieListActivity extends AppCompatActivity {
         movieListViewModel.searchMovieApi(query, pageNumber);
     }
 
-    private void GetRetrofitResponse() {
-        MovieApi movieApi = Servicey.getMovieApi();
+//    private void GetRetrofitResponse() {
+//        MovieApi movieApi = Servicey.getMovieApi();
+//
+//        Call<MovieSearchResponse> responseCall = movieApi
+//                .searchMovie(
+//                        Credentials.API_KEY,
+//                        "Jack Reacher",
+//                        1
+//                );
+//
+//        responseCall.enqueue(new Callback<MovieSearchResponse>() {
+//            @Override
+//            public void onResponse(Call<MovieSearchResponse> call, Response<MovieSearchResponse> response) {
+//                if(response.code() == 200){
+//                    Log.v("Tag", "the response" + response.body().toString());
+//
+//                    List<MovieModel> movies = new ArrayList<>(response.body().getMovies());
+//
+//                    for (MovieModel movie : movies) {
+//                        Log.v("Tag", "The release date" + movie.getRelease_date());
+//                    }
+//                } else {
+//                    try {
+//                        Log.v("Tag", "Error" + response.errorBody().string());
+//                    } catch (IOException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<MovieSearchResponse> call, Throwable t) {
+//
+//            }
+//        });
+//    }
 
-        Call<MovieSearchResponse> responseCall = movieApi
-                .searchMovie(
-                        Credentials.API_KEY,
-                        "Jack Reacher",
-                        1
-                );
-
-        responseCall.enqueue(new Callback<MovieSearchResponse>() {
-            @Override
-            public void onResponse(Call<MovieSearchResponse> call, Response<MovieSearchResponse> response) {
-                if(response.code() == 200){
-                    Log.v("Tag", "the response" + response.body().toString());
-
-                    List<MovieModel> movies = new ArrayList<>(response.body().getMovies());
-
-                    for (MovieModel movie : movies) {
-                        Log.v("Tag", "The release date" + movie.getRelease_date());
-                    }
-                } else {
-                    try {
-                        Log.v("Tag", "Error" + response.errorBody().string());
-                    } catch (IOException e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<MovieSearchResponse> call, Throwable t) {
-
-            }
-        });
-    }
-
-    private void GetRetrofitResponseAccordingToID() {
-        MovieApi movieApi = Servicey.getMovieApi();
-
-        Call<MovieModel> responseCall = movieApi
-                .getMovie(
-                        343611,
-                        Credentials.API_KEY
-                );
-
-        responseCall.enqueue(new Callback<MovieModel>() {
-            @Override
-            public void onResponse(Call<MovieModel> call, Response<MovieModel> response) {
-                if(response.code() == 200){
-
-                    MovieModel movie = response.body();
-
-                    Log.v("Tag: ", "The Response " + movie.getTitle());
-
-                } else {
-                    try {
-                        Log.v("Tag", "Error" + response.errorBody().string());
-                    } catch (IOException e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<MovieModel> call, Throwable t) {
-
-            }
-        });
-    }
+//    private void GetRetrofitResponseAccordingToID() {
+//        MovieApi movieApi = Servicey.getMovieApi();
+//
+//        Call<MovieModel> responseCall = movieApi
+//                .getMovie(
+//                        343611,
+//                        Credentials.API_KEY
+//                );
+//
+//        responseCall.enqueue(new Callback<MovieModel>() {
+//            @Override
+//            public void onResponse(Call<MovieModel> call, Response<MovieModel> response) {
+//                if(response.code() == 200){
+//
+//                    MovieModel movie = response.body();
+//
+//                    Log.v("Tag: ", "The Response " + movie.getTitle());
+//
+//                } else {
+//                    try {
+//                        Log.v("Tag", "Error" + response.errorBody().string());
+//                    } catch (IOException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<MovieModel> call, Throwable t) {
+//
+//            }
+//        });
+//    }
 
 }
