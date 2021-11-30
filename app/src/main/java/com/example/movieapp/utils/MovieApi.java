d631224ea1ecec2888d298da5f2d3a8f35fba79e
andrei <git1@email.com>
2021-11-30 12:52:52 +0100

    Added getMovie GET endpoint in MovieApi Interface

--- app/src/main/java/com/example/movieapp/utils/MovieApi.java
+++ app/src/main/java/com/example/movieapp/utils/MovieApi.java
@@ -1,9 +1,11 @@
 package com.example.movieapp.utils;
 
+import com.example.movieapp.models.MovieModel;
 import com.example.movieapp.response.MovieSearchResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface MovieApi {
@@ -15,6 +17,15 @@ public interface MovieApi {
             @Query("api_key") String key,
             @Query("query") String query,
             @Query("page") int page
+    );
+
+    // Making search with ID
+    // https://api.themoviedb.org/3/movie/550?api_key=52a18783ed514602a5facb15a0177e61
+    // Remember that movie_id= 550 is for Jack Reacher movie
+    @GET("movie/{movie_id}")
+    Call<MovieModel> getMovie(
+            @Path("movie_id") int movie_id,
+            @Query("api_key") String api_key
     );
 
 }
