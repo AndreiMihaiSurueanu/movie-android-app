9ece50e4c0f523e6c5335abc6cf9a9e9985d1859
andrei <git1@email.com>
2021-11-30 12:07:11 +0100

    Created searchMoviesApi method shell in MovieApiClient

--- app/src/main/java/com/example/movieapp/request/MovieApiClient.java
+++ app/src/main/java/com/example/movieapp/request/MovieApiClient.java
@@ -4,9 +4,12 @@ package com.example.movieapp.request;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.example.movieapp.AppExecutors;
 import com.example.movieapp.models.MovieModel;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class MovieApiClient {
 
@@ -28,6 +31,25 @@ public class MovieApiClient {
 
     public LiveData<List<MovieModel>> getMovies() {
         return mMovies;
+    }
+
+    public void searchMoviesApi() {
+
+        final Future myHandler = AppExecutors.getInstance().networkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+                // Retrieve Data from api
+
+            }
+        });
+
+        AppExecutors.getInstance().networkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                // Cancelling the retrofit call
+                myHandler.cancel(true);
+            }
+        }, 5000, TimeUnit.MICROSECONDS);
     }
 
 }
