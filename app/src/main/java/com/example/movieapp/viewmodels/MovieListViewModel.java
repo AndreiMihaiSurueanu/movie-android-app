e291e109fde7b76b721cc800feef0b726140a9f5
andrei <git1@email.com>
2021-11-30 10:11:01 +0100

    Updated MovieListViewModel to extend ViewModel

--- app/src/main/java/com/example/movieapp/viewmodels/MovieListViewModel.java
+++ app/src/main/java/com/example/movieapp/viewmodels/MovieListViewModel.java
@@ -2,12 +2,13 @@ package com.example.movieapp.viewmodels;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
 import com.example.movieapp.models.MovieModel;
 
 import java.util.List;
 
-public class MovieListViewModel {
+public class MovieListViewModel extends ViewModel {
 
     // this class is used for VIEWMODEL
 
